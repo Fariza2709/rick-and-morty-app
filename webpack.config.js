a5d75f19918632be05bf786930814b15809eae79
Fariza Zhakhansha <fakosh2709@gmail.com>
2024-08-17 13:02:10 +0500

    App

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: './main.js',
+  entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
